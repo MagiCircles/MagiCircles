7628cff387d3d68f2da2e18fbbd475fc45486b34
db0company <db0company@gmail.com>
2016-08-06 14:58:41 -0700

    Fix follow

--- web/notifications.py
+++ web/notifications.py
@@ -1,4 +1,5 @@
 from django.db.models import F
+from web.utils import join_data
 from web import models
 
 def pushNotification(user, notification_type, values, url_values=None, image=None):
@@ -8,8 +9,8 @@ def pushNotification(user, notification_type, values, url_values=None, image=Non
     notification = models.Notification.objects.create(
         owner=user,
         message=notification_type,
-        message_data=models._join_data(*values),
-        url_data=(models._join_data(*url_values) if url_values else None),
+        message_data=join_data(*values),
+        url_data=(join_data(*url_values) if url_values else None),
         image=image)
     models.UserPreferences.objects.filter(pk=user.preferences.pk).update(unread_notifications=F('unread_notifications') + 1)
     return notification
