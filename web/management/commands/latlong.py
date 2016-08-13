1b9d79782e66d9922d049aed080407acca2c4f50
db0company <db0company@gmail.com>
2016-08-13 12:25:55 -0700

    Never fail latlong

--- web/management/commands/latlong.py
+++ web/management/commands/latlong.py
@@ -44,16 +44,19 @@ def latlong(opt):
     mapcache = '{# this file is generated, do not edit it #}{% extends "base.html" %}{% load i18n %}{% load l10n %}{% block title %}{% trans "Map" %}{% endblock %}{% block content %}<div id="map"></div>{% endblock %}{% block afterjs %}{% localize off %}<script>var center=new google.maps.LatLng({% if center %}{{ center.latitude }},{{ center.longitude }}{% else %}30,0{% endif %});var zoom={% if zoom %}{{ zoom }}{% else %}2{% endif %};var addresses = ['
 
     for u in map:
-        mapcache += '{open}"username": "{username}","avatar": "{avatar}","location": "{location}","icon": "{icon}","latlong": new google.maps.LatLng({latitude},{longitude}){close},'.format(
-            open='{',
-            username=escape(u.user.username),
-            avatar=escape(models.avatar(u.user)),
-            location=escape(u.location),
-            icon=escape(u.favorite_character1_image if u.favorite_character1_image else 'static/img/default_map_icon.png'),
-            latitude=u.latitude,
-            longitude=u.longitude,
-            close='}',
-        )
+        try:
+            mapcache += '{open}"username": "{username}","avatar": "{avatar}","location": "{location}","icon": "{icon}","latlong": new google.maps.LatLng({latitude},{longitude}){close},'.format(
+                open='{',
+                username=escape(u.user.username),
+                avatar=escape(models.avatar(u.user)),
+                location=escape(u.location),
+                icon=escape(u.favorite_character1_image if u.favorite_character1_image else 'static/img/default_map_icon.png'),
+                latitude=u.latitude,
+                longitude=u.longitude,
+                close='}',
+            )
+        except:
+            print 'One user not added in map'
 
     mapcache += '];</script><script src="' + SITE_STATIC_URL + 'static/js/map.js"></script>{% endlocalize %}{% endblock %}'
 
