213f30864aeaf0044142997154024f5b464a4f74
db0company <db0company@gmail.com>
2016-09-16 05:09:24 -0700

    Add Cinderella Producers in official MagiCircles projects

--- web/raw.py
+++ web/raw.py
@@ -8,15 +8,21 @@ other_sites = [
         'url': 'http://schoolido.lu/',
     },
     {
-        'name': 'fr.gl',
-        'game_name': 'Glee Forever',
-        'image': 'http://fr.gl/static/img/avatar.png',
-        'url': 'http://fr.gl/',
-    },
-    {
         'name': 'Stardust Run',
         'game_name': 'Pokémon Go',
         'image': 'http://i.stardust.run/static/img/avatar.png',
         'url': 'http://stardust.run/',
+    },
+    {
+        'name': 'Cinderella Producers',
+        'game_name': 'THE iDOLM@STER Cinderella Girls: Starlight Stage',
+        'image': 'http://i.cinderella.pro/static/img/avatar.png',
+        'url': 'http://cinderella.pro/',
+    },
+    {
+        'name': 'fr.gl',
+        'game_name': 'Glee Forever',
+        'image': 'http://fr.gl/static/img/avatar.png',
+        'url': 'http://fr.gl/',
     },
 ]
