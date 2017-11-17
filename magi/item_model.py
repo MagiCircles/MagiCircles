0f4a9e50b7ce45c60ecc2252424199653f117b8f
Deby Lepage <db0company@gmail.com>
2017-11-17 15:44:19 +0800

    Provide a helper item model to inherit from that will save cached info related to owner foreign key

--- magi/item_model.py
+++ magi/item_model.py
@@ -351,3 +351,55 @@ class AccountAsOwnerModel(MagiModel):
 
     class Meta:
         abstract = True
+
+############################################################
+# CacheAccount
+
+class CacheOwner(MagiModel):
+    """
+    Will provide a cache with basic owner details
+    """
+    _cache_owner_days = 200
+    _cache_owner_last_update = models.DateTimeField(null=True)
+    _cache_owner_username = models.CharField(max_length=32, null=True)
+    _cache_owner_email = models.EmailField(blank=True)
+    _cache_owner_preferences_i_status = models.CharField(max_length=12, null=True)
+    _cache_owner_preferences_twitter = models.CharField(max_length=32, null=True, blank=True)
+    _cache_owner_color = models.CharField(max_length=100, null=True, blank=True)
+
+    def update_cache_owner(self):
+        """
+        Recommended to use select_related('owner', 'owner__preferences') when calling this method
+        """
+        self._cache_owner_last_update = timezone.now()
+        self._cache_owner_username = self.owner.username
+        self._cache_owner_email = self.owner.email
+        self._cache_owner_preferences_i_status = self.owner.preferences.i_status
+        self._cache_owner_preferences_twitter = self.owner.preferences.twitter
+        self._cache_owner_color = self.owner.preferences.color
+
+    def force_cache_owner(self):
+        self.update_cache_owner()
+        self.save()
+
+    @property
+    def cached_owner(self):
+        if not self._cache_owner_last_update or self._cache_owner_last_update < timezone.now() - datetime.timedelta(days=self._cache_owner_days):
+            self.force_cache_owner()
+        return AttrDict({
+            'pk': self.owner_id,
+            'id': self.owner_id,
+            'username': self._cache_owner_username,
+            'email': self._cache_owner_email,
+            'item_url': '/user/{}/{}/'.format(self.owner_id, self._cache_owner_username),
+            'ajax_item_url': '/ajax/user/{}/'.format(self.owner_id),
+            'preferences': AttrDict({
+                'i_status': self._cache_owner_preferences_i_status,
+                'status': dict(UserPreferences.STATUS_CHOICES)[self._cache_owner_preferences_i_status] if self._cache_owner_preferences_i_status else None,
+                'twitter': self._cache_owner_preferences_twitter,
+                'color': self._cache_owner_color,
+            }),
+        })
+
+    class Meta:
+        abstract = True
