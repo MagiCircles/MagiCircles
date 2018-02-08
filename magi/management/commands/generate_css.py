5d6a63e856c59470d9edb860f96fbf8f0e359897
Deby Lepage <db0company@gmail.com>
2018-02-08 16:46:05 +0800

    Fix generate_css on Windows

--- magi/management/commands/generate_css.py
+++ magi/management/commands/generate_css.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings as django_settings
 from magi.settings import USER_COLORS
@@ -9,7 +10,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         resource_package = 'magi'
-        resource_path = '/'.join(('static', 'less', 'per-color-generator.less'))
+        resource_path = os.path.join('static', 'less', 'per-color-generator.less')
         template = pkg_resources.resource_string(resource_package, resource_path)
 
         all_colors = u''
@@ -17,6 +18,6 @@ class Command(BaseCommand):
             all_colors += template.replace('HEX_COLOR', color[3]).replace('COLOR_NAME', color[0]).replace('COLOR', color[2])
         print all_colors
 
-        with open(django_settings.BASE_DIR + '/' + django_settings.SITE + '/static/less/generated_colors.less', 'w') as f:
+        with open(os.path.join(django_settings.BASE_DIR + '/' + django_settings.SITE + '/static/less/generated_colors.less', 'w+')) as f:
             f.write(all_colors.encode('UTF-8'))
             f.close()
