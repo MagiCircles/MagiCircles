9b557399380e39703697ede849bd8d6e38c25e69
db0company <db0company@gmail.com>
2019-08-20 00:07:31 +0200

    New default command import_data

---
+++ magi/management/commands/import_data.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+# Load dynamic module based on SITE
+
+import_data_module = __import__(settings.SITE + '.import_data', fromlist=[''])
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        options = ['verbose', 'local']
+        kwargs = { 'to_import': [] }
+        for arg in args:
+            if arg in options:
+                kwargs[arg] = True
+            else:
+                kwargs['to_import'] = arg
+        if not kwargs['to_import']:
+            kwargs['to_import'] = None
+
+        import_data_module.import_data(**kwargs)
