19823d0c490c2dfc8e1e9f018ff2cd66452989f3
db0company <db0company@gmail.com>
2020-01-09 08:18:46 +0100

    list of contest participants: use entry url as badge url

--- magi/management/commands/list_of_contest_participants.py
+++ magi/management/commands/list_of_contest_participants.py
@@ -383,7 +383,7 @@ class Command(BaseCommand):
         if name:
             print '# {}'.format(name)
             print ''
-        print '### **Thanks to everyone who participated and helped make this contest a success! We loved your entries!**'
+        print '### **Thanks to everyone who participated and helped make this event a success! We loved your entries!**'
         print ''
 
         if grid_instance:
@@ -494,20 +494,24 @@ class Command(BaseCommand):
                                 rank = models.Badge.RANK_GOLD
                                 name = name.replace(' - Participant', ' - Winner')
                         print 'Adding badge to {}'.format(username)
-                        user = models.User.objects.select_related('preferences').get(username=username)
-                        models.Badge.objects.create(
-                            owner=badge.owner,
-                            user=user,
-                            name=badge.name,
-                            description=badge.description,
-                            image=badge.image,
-                            url=badge.url,
-                            show_on_top_profile=badge.show_on_top_profile,
-                            show_on_profile=badge.show_on_profile,
-                            rank=rank,
-                        )
-                        user.preferences.force_update_cache('tabs_with_content')
-            '--list-missing-badges',
+                        try:
+                            user = models.User.objects.select_related('preferences').filter(username=username)[0]
+                        except IndexError:
+                            print '   User not found'
+                            user = None
+                        if user:
+                            models.Badge.objects.create(
+                                owner=badge.owner,
+                                user=user,
+                                name=badge.name,
+                                description=badge.description,
+                                image=badge.image,
+                                url=entry['url'],
+                                show_on_top_profile=badge.show_on_top_profile,
+                                show_on_profile=badge.show_on_profile,
+                                rank=rank,
+                            )
+                            user.preferences.force_update_cache('tabs_with_content')
         if self.options.get('list_missing_badges'):
             print json.dumps(cant_get, indent=4)
 
