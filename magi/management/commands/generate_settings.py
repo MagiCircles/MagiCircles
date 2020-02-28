1fc79a930808e620c38725d1e8fb26721329fa8f
db0company <db0company@gmail.com>
2020-02-28 16:32:07 +0100

    Fix generate_settings to make sure URLs are full

--- magi/management/commands/generate_settings.py
+++ magi/management/commands/generate_settings.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings as django_settings
+from magi import urls
 from magi.tools import generateSettings
+
 try:
     CUSTOM_GENERATE_SETTINGS = __import__(django_settings.SITE + '.management.commands.generate_settings', fromlist=[''])
 except ImportError:
