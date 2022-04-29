79837e45d06a8051c49e9baaf4173fb084c73ae2
Ian E <ian@teachable.com>
2022-04-29 15:22:26 -0600

    Type hinting, remove unneeded method call

--- magi/middleware/languageFromPreferences.py
+++ magi/middleware/languageFromPreferences.py
@@ -1,8 +1,9 @@
+from django.http import HttpRequest
 from django.utils import translation
 from django.utils.deprecation import MiddlewareMixin
 
 class LanguageFromPreferenceMiddleWare(MiddlewareMixin):
-    def process_request(self, request):
-        if request.user.is_authenticated and not request.user.is_anonymous():
+    def process_request(self, request: HttpRequest):
+        if request.user.is_authenticated and not request.user.is_anonymous:
             translation.activate(request.user.preferences.language)
             request.LANGUAGE_CODE = translation.get_language()
